4f4d58ed1db6f18bd8ad7b927b29b98bf5f941d5
Amirhosein Memarian <amirhoseinmrn1@gmail.com>
2026-04-27 17:28:12 +0330

    feat: add compress image extension

---
+++ Sources/ATOCR/Extensions/UIImage.swift
@@ -0,0 +1,46 @@
+//
+//  ImageExtensions.swift
+//  ATOCR
+//
+//  Created by Amir on 4/27/26.
+//
+import UIKit
+
+extension UIImage {
+    func compressedData(to maxSizeMB: Double) -> Data? {
+        let maxBytes = Int(maxSizeMB * 1024 * 1024)
+        var compression: CGFloat = 0.9
+        let minCompression: CGFloat = 0.1
+        guard var imageData = self.jpegData(compressionQuality: compression) else { return nil }
+        
+        while imageData.count > maxBytes && compression > minCompression {
+            compression -= 0.1
+            if let newData = self.jpegData(compressionQuality: compression) {
+                imageData = newData
+            } else {
+                break
+            }
+        }
+        if imageData.count > maxBytes {
+            var resizedImage = self
+            while imageData.count > maxBytes {
+                let scale: CGFloat = 0.8 // reduce each time by 20%
+                let newSize = CGSize(width: resizedImage.size.width * scale,
+                                     height: resizedImage.size.height * scale)
+                
+                UIGraphicsBeginImageContextWithOptions(newSize, false, resizedImage.scale)
+                resizedImage.draw(in: CGRect(origin: .zero, size: newSize))
+                let newImage = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                
+                guard let img = newImage,
+                      let newData = img.jpegData(compressionQuality: compression)
+                else { break }
+                
+                resizedImage = img
+                imageData = newData
+            }
+        }
+        return imageData
+    }
+}
