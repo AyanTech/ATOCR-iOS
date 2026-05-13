9159159afd81a0509bff9604b6c42a6f17fe0f63
Amirhosein Memarian <amirhoseinmrn1@gmail.com>
2026-05-13 14:09:29 +0330

    fix: fix checkmark in the spm bundle

--- Sources/ATOCR/OCRCollection/OCRCollectionViewCell.swift
+++ Sources/ATOCR/OCRCollection/OCRCollectionViewCell.swift
@@ -23,6 +23,7 @@ class OCRCollectionViewCell: UICollectionViewCell {
         self.titleLabel.text = data.title
         self.titleLabel.textColor = data.titleColor
         self.titleLabel.font = data.titleFont
+        self.checkmarkImage.image = UIImage.checkmark
         self.checkmarkImage.tintColor = .green
         self.containerView.layer.sublayers?.removeAll(where: { $0.name == "dashedBorderLayer" })
         let dashedLayer = CAShapeLayer()
