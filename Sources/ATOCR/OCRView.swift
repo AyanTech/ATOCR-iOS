9b9649f90c9fc419523666e6772898b4124231db
Amirhosein Memarian <amirhoseinmrn1@gmail.com>
2026-05-16 10:36:19 +0330

    feat: add title to ocr view

--- Sources/ATOCR/OCRView.swift
+++ Sources/ATOCR/OCRView.swift
@@ -12,86 +12,136 @@ public protocol OCRViewDelegate: AnyObject {
 }
 
 public final class OCRView: UIView {
-
+    
     public weak var delegate: OCRViewDelegate?
     private var items: [OCRCollectionItem] = []
-
+    
+    public var title: String = "" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    public var titleFont: UIFont = .systemFont(
+        ofSize: 18,
+        weight: .semibold
+    ) {
+        didSet {
+            titleLabel.font = titleFont
+        }
+    }
+    
+    public var titleColor: UIColor = .label {
+        didSet {
+            titleLabel.textColor = titleColor
+        }
+    }
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.setContentCompressionResistancePriority(
+            .required,
+            for: .vertical
+        )
+        label.setContentHuggingPriority(
+            .required,
+            for: .vertical
+        )
+        label.text = title
+        label.font = titleFont
+        label.textColor = titleColor
+        
+        return label
+    }()
+    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 16
-        layout.minimumInteritemSpacing = 8
-        layout.sectionInset = .init(
-            top: 16,
-            left: 16,
-            bottom: 16,
-            right: 16
-        )
-
+        
         let view = UICollectionView(
             frame: .zero,
             collectionViewLayout: layout
         )
-
+        
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .clear
-        view.delegate = self
-        view.dataSource = self
-
-        OCRCollectionViewCell.register(for: view)
-
+        
         return view
     }()
-
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI()
+        setupView()
     }
-
+    
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    convenience init(
+        title: String,
+        titleFont: UIFont = .systemFont(
+            ofSize: 18,
+            weight: .semibold
+        ),
+        titleColor: UIColor = .label
+    ) {
+        self.init(frame: .zero)
+        
+        self.title = title
+        self.titleFont = titleFont
+        self.titleColor = titleColor
+    }
+    
+    private func setupView() {
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            titleLabel,
+            collectionView
+        ])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
 
 public extension OCRView {
-
     func setItems(_ items: [OCRCollectionItem]) {
         self.items = items
         collectionView.reloadData()
     }
 }
 
-private extension OCRView {
-
-    func setupUI() {
-        addSubview(collectionView)
-
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-    }
-}
-
 extension OCRView: UICollectionViewDataSource {
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                numberOfItemsInSection section: Int) -> Int {
-        items.count
+        return items.count
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
+        
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OCRCollectionViewCell.nameOfClass,
                                                             for: indexPath) as? OCRCollectionViewCell else {
             return UICollectionViewCell()
         }
-
+        
         cell.config(data: items[indexPath.item])
-
+        
         return cell
     }
 }
@@ -103,22 +153,22 @@ extension OCRView: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate 
         let item = items[indexPath.item]
         delegate?.ocrViewDidSelectItem(item)
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView,
                                layout collectionViewLayout: UICollectionViewLayout,
                                sizeForItemAt indexPath: IndexPath) -> CGSize {
-
+        
         let itemsPerRow: CGFloat = 4
         let spacing: CGFloat = 8
         let horizontalInset: CGFloat = 32
-
+        
         let totalSpacing =
         ((itemsPerRow - 1) * spacing) + horizontalInset
-
+        
         let width =
         (collectionView.bounds.width - totalSpacing)
         / itemsPerRow
-
+        
         return CGSize(width: width, height: width + 24)
     }
 }
