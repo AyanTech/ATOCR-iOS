3bf87367441e86c7e27503e02abed3bbeac7706d
Amirhosein Memarian <amirhoseinmrn1@gmail.com>
2026-05-17 13:54:42 +0330

    fix: add stop loading when pending

--- Sources/ATOCR/Services/GetCardOcrResult/GetCardOcrResultPO.swift
+++ Sources/ATOCR/Services/GetCardOcrResult/GetCardOcrResultPO.swift
@@ -24,7 +24,7 @@ public extension GetCardOcrResultPO {
         token: String
     ) {
         emit(.isLoading(getCardOcrResultRequest, true))
-
+        
         getCardOcrResultRequest = AppNetwork.shared.post(
             url: url,
             input: input,
@@ -35,31 +35,31 @@ public extension GetCardOcrResultPO {
             ) in
                 
                 guard let self else { return }
-
+                
                 if let error {
                     self.emit(.isLoading(self.getCardOcrResultRequest, false))
                     self.emit(.didError(error.persianDescription ?? "خطا در دریافت اطلاعات"))
                     return
                 }
-
+                
                 guard let response else {
                     self.emit(.isLoading(self.getCardOcrResultRequest, false))
                     self.emit(.didError("خطا در دریافت اطلاعات"))
                     return
                 }
-
+                
                 switch response.status.lowercased() {
                 case "failed":
                     self.emit(.isLoading(self.getCardOcrResultRequest, false))
                     self.emit(.didError(response.description ?? ""))
-
+                    
                 case "pending":
+                    self.emit(.isLoading(self.getCardOcrResultRequest, false))
                     let delay = Double(response.nextCallInterval ?? 0) / 1000.0
-
                     DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
                         self?.getCardOcrResult(url: url, input: input, token: token)
                     }
-
+                    
                 default:
                     self.emit(.isLoading(self.getCardOcrResultRequest, false))
                     self.getCardOcrResultResponse = response
@@ -68,7 +68,7 @@ public extension GetCardOcrResultPO {
             }
         )
     }
-
+    
     func emit(_ change: OCRChangeHandler) {
         getCardOcrResultChangeHandler?(change)
     }
