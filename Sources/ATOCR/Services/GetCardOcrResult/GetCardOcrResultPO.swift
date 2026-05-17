db1fc33454e25cd9b95b75a9aec0c2eaff0e1840
Amirhosein Memarian <amirhoseinmrn1@gmail.com>
2026-05-17 13:39:54 +0330

    fix: fix main thread of request

--- Sources/ATOCR/Services/GetCardOcrResult/GetCardOcrResultPO.swift
+++ Sources/ATOCR/Services/GetCardOcrResult/GetCardOcrResultPO.swift
@@ -16,24 +16,59 @@ public protocol GetCardOcrResultPO: AnyObject {
     func getCardOcrResult(url: String, input: GetCardOcrResultInput, token: String)
 }
 
+@MainActor
 public extension GetCardOcrResultPO {
-    func getCardOcrResult(url: String, input: GetCardOcrResultInput, token: String) {
+    func getCardOcrResult(
+        url: String,
+        input: GetCardOcrResultInput,
+        token: String
+    ) {
         emit(.isLoading(getCardOcrResultRequest, true))
-        getCardOcrResultRequest = AppNetwork.shared.post(url: url,
-                                                         input: input,
-                                                         token: token,
-                                                         completionHandler: ({ (response: GetCardOcrResultResponse?,
-                                                                                error: ATPError?) in
-            self.emit(.isLoading(self.getCardOcrResultRequest, false))
-            if let error {
-                self.emit(.didError(error.persianDescription ?? "خطا در دریافت اطلاعات"))
-            } else {
-                self.getCardOcrResultResponse = response
-                self.emit(.didSuccess)
+
+        getCardOcrResultRequest = AppNetwork.shared.post(
+            url: url,
+            input: input,
+            token: token,
+            completionHandler: { [weak self] (
+                response: GetCardOcrResultResponse?,
+                error: ATPError?
+            ) in
+                
+                guard let self else { return }
+
+                if let error {
+                    self.emit(.isLoading(self.getCardOcrResultRequest, false))
+                    self.emit(.didError(error.persianDescription ?? "خطا در دریافت اطلاعات"))
+                    return
+                }
+
+                guard let response else {
+                    self.emit(.isLoading(self.getCardOcrResultRequest, false))
+                    self.emit(.didError("خطا در دریافت اطلاعات"))
+                    return
+                }
+
+                switch response.status.lowercased() {
+                case "failed":
+                    self.emit(.isLoading(self.getCardOcrResultRequest, false))
+                    self.emit(.didError(response.description ?? ""))
+
+                case "pending":
+                    let delay = Double(response.nextCallInterval ?? 0) / 1000.0
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                        self?.getCardOcrResult(url: url, input: input, token: token)
+                    }
+
+                default:
+                    self.emit(.isLoading(self.getCardOcrResultRequest, false))
+                    self.getCardOcrResultResponse = response
+                    self.emit(.didSuccess)
+                }
             }
-        }))
+        )
     }
-    
+
     func emit(_ change: OCRChangeHandler) {
         getCardOcrResultChangeHandler?(change)
     }
